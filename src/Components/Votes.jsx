c2286089ea1486276ffbaa854bf571ef5b55d4ca
FelicityRC <felicitycarleton@hotmail.com>
2022-11-04 08:50:53 +0000

    add Votes component to allow a user to add/remove vote

---
+++ src/Components/Votes.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import * as api from "../api";
+
+const Votes = ({ review_id, votes }) => {
+  const [userVotes, setUserVotes] = useState(0);
+console.log(review_id, votes, "console log")
+  const handleUpVotes = () => {
+    if (userVotes === 0) {
+      setUserVotes(1);
+      api.patchVotes(review_id, 1);
+    } else if (userVotes === -1) {
+      setUserVotes(0);
+      api.patchVotes(review_id, 1);
+    }
+  };
+
+  const handleDownVotes = () => {
+    if (userVotes === 0) {
+      setUserVotes(-1);
+      api.patchVotes(review_id, -1);
+    } else if (userVotes === 1) {
+      setUserVotes(0);
+      api.patchVotes(review_id, -1);
+    }
+  };
+
+  return (
+    <section>
+      <p style={{ fontStyle: "italic" }}>Votes: {votes + userVotes}</p>
+      <button className="thumbsUp" onClick={handleUpVotes} disabled={userVotes === 1 ? true : false}>      
+      👍</button>
+      <button className="thumbsDown" onClick={handleDownVotes} disabled={userVotes === -1 ? true : false}>👎</button>
+    </section>
+  );
+};
+
+export default Votes;
