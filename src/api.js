aa249bdb1f197454424d11a6722c8498c234b3e9
FelicityRC <felicitycarleton@hotmail.com>
2022-11-12 14:48:11 +0000

    ammend baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// "http://localhost:9090/api"
-// "https://nc-games-project5.herokuapp.com/api"
+//"http://localhost:9090/api"
+//"https://nc-games-project5.herokuapp.com/api"
 
 const myApi = axios.create({
   baseURL: "https://fair-plum-dog-sari.cyclic.app/api",
